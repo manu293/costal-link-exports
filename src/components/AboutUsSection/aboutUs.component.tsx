2849ca84a1808e3b3d8b1011fc7c9f058dc9dbc0
Manoj S M <manojsm293@gmail.com>
2024-06-23 13:25:30 +0530

    chore: Update About Us Section Content

--- src/components/AboutUsSection/aboutUs.component.tsx
+++ src/components/AboutUsSection/aboutUs.component.tsx
@@ -11,14 +11,16 @@ import "./aboutUs.styles.css";
 const AboutUsSection = () => {
   return (
     <div>
-      <h1 className="aboutUsSectionHeader">
-        Efficient Cargo Delivery Worldwide
-      </h1>
+      <h1 className="aboutUsSectionHeader">Welcome to Coastal Link Exports</h1>
       <p className="aboutUsSectionSubHeader">
-        As a certified member the Agricultural and Processed Food Products
-        Export Development Authority (APEDA) we take pride in offering you the
-        finest quality products, meticulously processed to meet international
-        standards.
+        Your Premier Source for Spices and its derivatives, Coconut and its
+        derivatives, Fresh fruits and vegetables, Seafood and a wide range of
+        processed food. Coastal Link Exports was born out of a passion for
+        developing exports from the coastal region of Karnataka. Founded by a
+        team of experienced individuals in the domain of export, trading and
+        finance, our company is dedicated to promoting exports from the coastal
+        regions as well as from the entire State of Karnataka while upholding
+        the highest standards of quality and commitment to our buyers.
       </p>
       <div className="aboutUsImageContainer">
         <Image
