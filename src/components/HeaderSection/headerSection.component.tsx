0ab238c8408e8948fc5d2d1140b2cb72ea9600a1
Manoj S M <manojsm293@gmail.com>
2024-06-23 12:35:45 +0530

    feat: Add Header Section

---
+++ src/components/HeaderSection/headerSection.component.tsx
@@ -0,0 +1,38 @@
+// imports
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+// local imports
+import costalLinkExports from "../../../public/images/logo.png";
+
+// styles
+import "./headerSection.styles.css";
+
+const HeaderSection = () => {
+  return (
+    <header className="headerSection">
+      <a href="./" className="headerTitle">
+        <Image
+          fill
+          className="headerImage"
+          alt="Costal Link Exports"
+          src={costalLinkExports}
+        />
+      </a>
+      <div className="headerLinkContainer">
+        <Link className="headerLinkText" href="#commitment">
+          Commitment
+        </Link>
+        <Link className="headerLinkText" href="#partners">
+          Partners
+        </Link>
+        <Link className="headerLinkText" href="#contact">
+          Contact
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderSection;
