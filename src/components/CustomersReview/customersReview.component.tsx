dac4dbfa90c9ae36376aae6d5ad7e1de6ab945e3
Manoj S M <manojsm293@gmail.com>
2024-06-30 18:19:27 +0530

    chore: Refactor And Update Customers Review Section

--- src/components/CustomersReview/customersReview.component.tsx
+++ src/components/CustomersReview/customersReview.component.tsx
@@ -1,50 +1,41 @@
 // imports
 import React from "react";
-import Image from "next/image";
-
-// local imports
-import ProfilePhoto from "../../../public/images/profile.webp";
 
 // styles
 import "./customersReview.styles.css";
 
 const CustomerReviews = () => {
   return (
-    <div className="customersReviewContainer">
-      <h1 className="customersReviewHeader">
-        What They Say
-        <span className="customerReviewBreakText" />
-        About Us
-      </h1>
+    <>
+      <div className="customersReviewContainer">
+        <h1 className="customersReviewHeader">About Us</h1>
 
-      <div className="customersReviewSection">
-        <div className="customerReviewProfileSection">
-          <div className="customerReviewProfileImageContainer">
-            <Image
-              fill
-              alt="cargos"
-              src={ProfilePhoto}
-              className="customerReviewProfileImage"
-            />
+        <div className="customersReviewSection">
+          <div className="customerReviewSection">
+            <h3 className="customerReviewSubSection">Company Background</h3>
+            <span className="customerReviewText">
+              Coastal Link Exports was born out of a passion for developing
+              exports from the coastal region of Karnataka. Founded by a team of
+              experienced individuals in the domains of export, trading, and
+              finance, our company is dedicated to promoting exports from the
+              coastal regions as well as from the entire State of Karnataka
+              while upholding the highest standards of quality and commitment to
+              our buyers.
+            </span>
           </div>
-          <div className="customerReviewProfileNameContainer">
-            <h3 className="customerReviewProfileNameText">Dennis White</h3>
-            <p className="customerReviewProfileNameSubText">California Cargo</p>
-          </div>
-        </div>
-        <div className="customerReviewSection">
-          <span className="customerReviewText">
-            I have been working with Coastal Link Exports for over a year now,
-            and I couldn&rsquo;t be more satisfied with their services. Their
-            commitment to quality and timely delivery is unparalleled. The team
-            is professional, responsive, and always goes the extra mile to
-            ensure our shipments arrive safely and on time. I highly recommend
-            Coastal Link Exports to anyone looking for a reliable and efficient
-            export partner.
-          </span>
         </div>
       </div>
-    </div>
+
+      <div>
+        <h1 className="customerCertificationTitle">Certifications</h1>
+        <p className="customerCertificationSubInfoSubSection">
+          As a certified member of the Agricultural and Processed Food Products
+          Export Development Authority (APEDA), we take pride in offering you
+          the finest quality products, meticulously processed to meet
+          international standards.
+        </p>
+      </div>
+    </>
   );
 };
 
