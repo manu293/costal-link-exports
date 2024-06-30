42ee805c456d23d77e79a0c540dd4cab3d92f1dc
Manoj S M <manojsm293@gmail.com>
2024-06-30 18:17:17 +0530

    chore: Refactor And Copy Changes In Commitment Section

--- src/components/CommitmentSection/commitmentSection.component.tsx
+++ src/components/CommitmentSection/commitmentSection.component.tsx
@@ -1,9 +1,21 @@
 // imports
 import React from "react";
 import Image from "next/image";
+import { BiSolidTShirt } from "react-icons/bi";
+import { MdEngineering } from "react-icons/md";
+import { MdElectricBolt } from "react-icons/md";
+import { FaSprayCanSparkles } from "react-icons/fa6";
 
 // local imports
 import cargoBgImage from "../../../public/images/cargos.webp";
+import product1Image from "../../../public/images/product_display_1.webp";
+import product2Image from "../../../public/images/product_display_2.webp";
+import product3Image from "../../../public/images/product_display_3.webp";
+import product4Image from "../../../public/images/product_display_4.webp";
+import product5Image from "../../../public/images/product_display_5.webp";
+import product6Image from "../../../public/images/product_display_6.webp";
+import product7Image from "../../../public/images/product_display_7.webp";
+import product8Image from "../../../public/images/product_display_8.webp";
 
 // styles
 import "./commitmentSection.styles.css";
@@ -23,27 +35,117 @@ const CommitmentSection = () => {
         />
       </div>
       <h3 className="commitmentSubInfoSection">
-        Ensure timely delivery with
+        Partnership
         <br />
-        Utmost Professionalism
       </h3>
       <p className="commitmentSubInfoSubSection">
-        As a certified member the Agricultural and Processed Food Products
-        Export Development Authority (APEDA) we take pride in offering you the
-        finest quality products, meticulously processed to meet international
-        standards. Coastal Link Exports is guided and advised by K Ravindran who
-        has vast experience in international trade for several decades. He has
-        huge export performance behind him and has contributed to export of
-        agricultural commodities, engineering goods, garments and textiles,
-        FMCG, plastic and allied products, chemicals, etc. Apart from
-        agricultural commodities and seafood, Coastal Link Exports has tie up
-        with MSME manufacturers in the following sectors.
+        Apart from agricultural commodities and seafood, Coastal Link Exports
+        has tie-ups with MSME manufacturers in the following sectors:
       </p>
       <div className="commitmentSectorsContainer">
-        <div className="commitmentSectors">Plastic goods</div>
-        <div className="commitmentSectors">Engineering goods</div>
-        <div className="commitmentSectors">Electrical goods</div>
-        <div className="commitmentSectors">Garments and textiles</div>
+        <div className="commitmentSectors">
+          <div className="commitmentSectorsCircle">
+            <FaSprayCanSparkles color="#fff" size={30} />
+          </div>
+          <p className="commitmentSectorsSubTitle">Plastic goods</p>
+        </div>
+        <div className="commitmentSectors">
+          <div className="commitmentSectorsCircle">
+            <MdEngineering color="#fff" size={30} />
+          </div>
+          <p className="commitmentSectorsSubTitle">Engineering goods</p>
+        </div>
+        <div className="commitmentSectors">
+          <div className="commitmentSectorsCircle">
+            <MdElectricBolt color="#fff" size={30} />
+          </div>
+          <p className="commitmentSectorsSubTitle">Electrical goods</p>
+        </div>
+        <div className="commitmentSectors">
+          <div className="commitmentSectorsCircle">
+            <BiSolidTShirt color="#fff" size={30} />
+          </div>
+          <p className="commitmentSectorsSubTitle">Garment goods</p>
+        </div>
+      </div>
+      <p className="commitmentSubInfoSubSection">
+        We have their mandate to promote exports of their manufactured products.
+        Mangaluru is well connected by road, air, and sea, and therefore we
+        export sourced products from across the State of Karnataka through
+        Mangaluru airport and seaport. Mangaluru is well connected by road from
+        different districts of Karnataka and Kerala, making it easy for the
+        products meant for export to reach safely and quickly
+      </p>
+
+      <h3 className="commitmentSubInfoSection2">Products</h3>
+      <div className="commitmentProductsSection">
+        <div className="commitmentProductContainer">
+          <Image
+            src={product1Image}
+            alt="spices and derivatives"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Spices And Derivatives</h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product2Image}
+            alt="Coconut and derivatives"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Coconut and derivatives</h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product3Image}
+            alt="Fresh fruits and vegetables"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">
+            Fresh fruits and vegetables
+          </h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product4Image}
+            alt="Sea And Processed food"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Sea And Processed food</h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product5Image}
+            alt="Engineering goods"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Engineering goods</h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product8Image}
+            alt="Garments and textiles"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Garments and textiles</h4>
+        </div>
+        <div className="commitmentProductContainer">
+          <Image
+            src={product6Image}
+            alt="Electrical goods"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Electrical goods</h4>
+        </div>
+
+        <div className="commitmentProductContainer">
+          <Image
+            src={product7Image}
+            alt="Plastic goods"
+            className="commitmentProductImage"
+          />
+          <h4 className="commitmentProductSubTitle">Plastic goods</h4>
+        </div>
       </div>
     </div>
   );
