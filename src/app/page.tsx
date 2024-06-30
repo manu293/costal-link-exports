59fd82864d38a948bfe30ab4cf0fdffa35af7ecf
Manoj S M <manojsm293@gmail.com>
2024-06-30 18:16:24 +0530

    chore: Remove And Reorder Milestone Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,18 @@ import FooterSection from "@/components/FooterSection/footerSection.component";
 import ServiceSection from "@/components/ServicesSection/servicesSection.component";
 import PartnersSection from "@/components/PartnersSection/partnersSection.component";
 import CustomerReviews from "@/components/CustomersReview/customersReview.component";
-import MileStonesSection from "@/components/MileStoneSection/mileStoneSection.component";
 import CommitmentSection from "@/components/CommitmentSection/commitmentSection.component";
 
 export default function Home() {
   return (
     <>
       <HeaderSection />
+      <AboutUsSection />
       <main className="mainContainer">
-        <AboutUsSection />
+        <CustomerReviews />
         <CommitmentSection />
-        <MileStonesSection />
         <ServiceSection />
         <PartnersSection />
-        <CustomerReviews />
       </main>
       <FooterSection />
     </>
