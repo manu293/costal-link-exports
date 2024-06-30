d3cae272844690001cdf4b73f4e8c08fb5684605
Manoj S M <manojsm293@gmail.com>
2024-06-30 18:16:42 +0530

    chore: Update About Us Section Text And Style

--- src/components/AboutUsSection/aboutUs.component.tsx
+++ src/components/AboutUsSection/aboutUs.component.tsx
@@ -1,34 +1,20 @@
 // imports
 import React from "react";
-import Image from "next/image";
-
-// local imports
-import efficientCargoImage from "../../../public/images/efficient_cargo.jpg";
 
 // styles
 import "./aboutUs.styles.css";
 
 const AboutUsSection = () => {
   return (
-    <div>
+    <div className="aboutUsSection">
+      <div className="aboutUsSectionOverlay" />
       <h1 className="aboutUsSectionHeader">Welcome to Coastal Link Exports</h1>
-      <p className="aboutUsSectionSubHeader">
-        Your Premier Source for Spices and its derivatives, Coconut and its
-        derivatives, Fresh fruits and vegetables, Seafood and a wide range of
-        processed food. Coastal Link Exports was born out of a passion for
-        developing exports from the coastal region of Karnataka. Founded by a
-        team of experienced individuals in the domain of export, trading and
-        finance, our company is dedicated to promoting exports from the coastal
-        regions as well as from the entire State of Karnataka while upholding
-        the highest standards of quality and commitment to our buyers.
-      </p>
-      <div className="aboutUsImageContainer">
-        <Image
-          fill
-          src={efficientCargoImage}
-          alt="efficient cargo shipping"
-          className="serviceInfoRoadShipping"
-        />
+      <div className="aboutUsSectionSubHeaderContainer">
+        <p className="aboutUsSectionSubHeader">
+          Your Premier Source for Spices and its derivatives, Coconut and its
+          derivatives, Fresh fruits and vegetables, Seafood, and a wide range of
+          processed food.
+        </p>
       </div>
     </div>
   );
