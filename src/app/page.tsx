0bc6906f2409bb83f0d574db74113a19ef62e55b
Manoj S M <manojsm293@gmail.com>
2024-07-02 13:33:27 +0530

    chore: Remove Service Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import AboutUsSection from "@/components/AboutUsSection/aboutUs.component";
 import HeaderSection from "@/components/HeaderSection/headerSection.component";
 import FooterSection from "@/components/FooterSection/footerSection.component";
-import ServiceSection from "@/components/ServicesSection/servicesSection.component";
 import PartnersSection from "@/components/PartnersSection/partnersSection.component";
 import CustomerReviews from "@/components/CustomersReview/customersReview.component";
 import CommitmentSection from "@/components/CommitmentSection/commitmentSection.component";
@@ -15,7 +14,6 @@ export default function Home() {
       <main className="mainContainer">
         <CustomerReviews />
         <CommitmentSection />
-        <ServiceSection />
         <PartnersSection />
       </main>
       <FooterSection />
