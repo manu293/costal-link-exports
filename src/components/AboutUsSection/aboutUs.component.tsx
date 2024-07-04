c5c7d11348972489437080548c93111f73f33a06
Manoj S M <manojsm293@gmail.com>
2024-07-04 09:39:13 +0530

    chore: Reduce Header Font Size And Copy Changes

--- src/components/AboutUsSection/aboutUs.component.tsx
+++ src/components/AboutUsSection/aboutUs.component.tsx
@@ -8,7 +8,7 @@ const AboutUsSection = () => {
   return (
     <div className="aboutUsSection">
       <div className="aboutUsSectionOverlay" />
-      <h1 className="aboutUsSectionHeader">Welcome to Coastal Link Exports</h1>
+      <h1 className="aboutUsSectionHeader">Welcome to CoastalLink Exports</h1>
       <div className="aboutUsSectionSubHeaderContainer">
         <p className="aboutUsSectionSubHeader">
           Your Premier Source for Spices and its derivatives, Coconut and its
